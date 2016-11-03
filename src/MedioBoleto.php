af70d83e23618a5f16bfa5e2e6c09212abe39fbc
Agustín López <lopezagus98@gmail.com>
2016-11-03 00:12:01 -0300

    Update MedioBoleto.php

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -6,9 +6,9 @@ class MedioBoleto extends Tarjeta {
 	public function __construct($id) {
 		$this->id = $id;
 		$this->viajesPlus = 0;
-		$this->boletoColectivo = 4.25;
+		$this->boletoColectivo = 4;
 		$this->boletoBici = 6;
 		$this->saldo = 0;
-		$this->trasbordo = 1.32;
+		$this->trasbordo = (float)((int)($this->boletoColectivo/3*100)/100);
 	}	
 }
